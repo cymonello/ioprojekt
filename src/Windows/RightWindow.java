753aa5ad305d938505f8d2854d7e5d851d9e21e6
BoyFarmer <BoyFarmerB@gmail.com>
2016-03-23 21:31:25 +0100

    Merge origin/master
    Conflicts:
    	src/Windows/MainWindow.java

--- src/Windows/RightWindow.java
+++ src/Windows/RightWindow.java
@@ -11,17 +11,22 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
 import javax.swing.BorderFactory;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.JTableHeader;
 
 /**
  * To jest klasa odpowiedzialna za poprawne wyświetlanie prawej strony naszego
@@ -33,6 +38,7 @@ import javax.swing.JTextField;
  * @author Bartłomiej
  */
 public class RightWindow extends JPanel implements ActionListener {
+
     JTable jTable = new JTable();
 
     RightWindow() {
@@ -44,31 +50,30 @@ public class RightWindow extends JPanel implements ActionListener {
         setLayout(null);
         setBounds(WindowConstants.BORDER, 0, WindowConstants.WIDTH - WindowConstants.BORDER, WindowConstants.HEIGHT);
         setBackground(Color.black);
-        
+
         repaint();
     }
 
     public void MakeRepertoire() {
         JComboBox jcbDate;
-        jTable = null;
-        
+
         removeAll();
         setLayout(null);
         setBounds(WindowConstants.BORDER, 0, WindowConstants.WIDTH - WindowConstants.BORDER, WindowConstants.HEIGHT);
         setBackground(Color.black);
-        
+
         JLabel jlChooseDate = new JLabel("WYBIERZ DATE:");
-        
+
         jlChooseDate.setFont(new Font("Arial Black", Font.CENTER_BASELINE, 17));
         jlChooseDate.setBounds(10, 30, 180, 30);
         jlChooseDate.setForeground(Color.white);
         add(jlChooseDate);
-        
+
         jcbDate = new JComboBox();
         SimpleDateFormat ft = new SimpleDateFormat("dd.MM.yy");
         Calendar cal = Calendar.getInstance();
         jcbDate.addItem(ft.format(cal.getTime()));
-        
+
         for (int i = 0; i < 5; i++) {
             cal.add(Calendar.DAY_OF_MONTH, 1);
             java.util.Date data = cal.getTime();
@@ -78,26 +83,64 @@ public class RightWindow extends JPanel implements ActionListener {
         jcbDate.setFont(new Font("Arial Black", Font.CENTER_BASELINE, 17));
         jcbDate.setForeground(Color.GRAY);
         jcbDate.setBackground(Color.CYAN);
-       /* jcbDate.addActionListener(new ActionListener() {
+
+        jcbDate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //JComboBox source = (JComboBox) e.getSource();
-                //String str = (String) source.getSelectedItem();
-                //System.out.println(str);
-                //try {
-                   // Repertoire rep = new Repertoire(str);
-                    //String[][] tab = rep.getValue();
-                    //String[] str1 = {"Tytuł","Wiek","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17"};
-                    //jTable = new JTable(tab,str1);
-                    //jTable.setBounds(10,100,980,490);
-                    
-                    //MainWindow.rightPanel.add(jTable);
-                //} catch (ClassNotFoundException ex) {
-                //} catch (SQLException ex){
-               // }
+                JComboBox source = (JComboBox) e.getSource();
+                String str = (String) source.getSelectedItem();
+                try {
+                    Repertoire rep = new Repertoire(str);
+                    String[][] tab = rep.getValue();
+
+                    String[] tab1 = {"Tytuł", "Wiek", "??", "Język", "Czas", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"};
+
+                    jTable = new JTable(tab, tab1);
+                    jTable.setEnabled(false);
+
+                    JTableHeader header = jTable.getTableHeader();
+                    header.setBackground(new Color(128, 17, 17));
+                    header.setForeground(new Color(255, 227, 227));
+                    jTable.setBackground(new Color(128, 17, 17));
+                    jTable.setForeground(new Color(255, 227, 227));
+                    jTable.addMouseListener(new MouseListener() {
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+                            int row = jTable.rowAtPoint(e.getPoint());
+                            int col = jTable.columnAtPoint(e.getPoint());
+                            System.out.println(row + " " + col);
+                        }
+
+                        @Override
+                        public void mousePressed(MouseEvent e) {
+                        }
+
+                        @Override
+                        public void mouseReleased(MouseEvent e) {
+                        }
+
+                        @Override
+                        public void mouseEntered(MouseEvent e) {
+                        }
+
+                        @Override
+                        public void mouseExited(MouseEvent e) {
+                        }
+                    });
+                    for (int i = 0; i < jTable.getRowCount(); i++) {
+                        jTable.setRowHeight(i, 15);
+                    }
+                    JScrollPane scrollPane = new JScrollPane(jTable);
+                    scrollPane.setBounds(10, 150, 780, jTable.getRowCount() * jTable.getRowHeight() + 22);
+                    MainWindow.rightPanel.add(scrollPane);
+                    MainWindow.rightPanel.repaint();
+                    //MakeRepertoireWithTab(rep.getValue());
+
+                } catch (ClassNotFoundException | SQLException ex) {
+                }
             }
 
-        });*/
+        });
         add(jcbDate);
         repaint();
     }
@@ -108,7 +151,9 @@ public class RightWindow extends JPanel implements ActionListener {
         setBackground(Color.BLACK);
         setBounds(WindowConstants.BORDER, 0, WindowConstants.WIDTH - WindowConstants.BORDER, WindowConstants.HEIGHT);
 
-        JTextField jtfSearch = new JTextField("Szukaj filmu");
+        JComboBox<String> jtfSearch = new JComboBox<>();
+        String str[] = {"1","2","3"};
+        jtfSearch.setModel(new DefaultComboBoxModel(str));
         jtfSearch.setBounds(150, (int) (WindowConstants.HEIGHT * 0.3), WindowConstants.WIDTH - WindowConstants.BORDER - 300, 100);
         jtfSearch.setFont(new Font("Arial Black", Font.CENTER_BASELINE, 36));
         jtfSearch.setForeground(Color.WHITE);
