503042491e106c592b1fea53962f39ffe6201179
deleviretta <przedostatni_bastion@o2.pl>
2016-03-23 19:28:30 +0100

    test

--- src/Repertoire/Repertoire.java
+++ src/Repertoire/Repertoire.java
@@ -6,5 +6,15 @@ import database.MoviesDB;
  * Created by deleviretta on 23.03.16.
  */
 public class Repertoire {
-    MoviesDB mdb;
+    public void Movie(){
+        MoviesDB mdb = new MoviesDB();
+        mdb.open();
+
+        mdb.close();
+    }
+    String dateOfMovies;
+
+    public Repertoire(String dateOfMovies) {
+        this.dateOfMovies = dateOfMovies;
+    }
 }
