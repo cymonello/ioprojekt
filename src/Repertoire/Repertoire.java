680539c3a8a261e9810de0f609ecb178ef6d2f5b
deleviretta <przedostatni_bastion@o2.pl>
2016-03-26 01:23:54 +0100

    test

--- src/Repertoire/Repertoire.java
+++ src/Repertoire/Repertoire.java
@@ -1,42 +1,95 @@
 package Repertoire;
 
-import database.MoviesDB;
 import database.TermsDB;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashMap;
+
 
 /**
- * Created by deleviretta on 23.03.16.
+ * Created on 23.03.16.
  */
 public class Repertoire {
-    String dateOfMovie;
-    ResultSet movies;
-    String[][] repertoire;
+    ResultSet movies; //referencja do ResultSet z TermsDB
+    String[] movieInfo; //wynikowa tablica z info o filmie
     TermsDB tdb;
+    String date; //data
+    Integer hall; //sala
+    Integer[] hours; // godzina filmu
+    Integer[] movie; //tablica id filmow na dany dzien
+    Integer id; //przechwowuje id terms
+    // tablica asocjacyjna przechowujaca informacje o filmie, klucz movie(id), wartość movieInfo
+    HashMap<Integer, String[]> mapMovieInfo = new HashMap<>();
+    // tablica asocjacyjna przechowujaca godziny dla danego filmu, klucz - id filmu, wartość tablica z godzinami
+    HashMap<Integer, Integer[]> mapHours = new HashMap<>();
+    /**
+     * Konstruktor przyjmujący date filmu, tworzy ResultSet dla bazy Terms, skąd zostaną następnie pobrane ID filmów
+     * przyjmuje datę jako String
+     *
+     * @param dateOfMovies data filmu
+     * @throws ClassNotFoundException
+     */
+
 
     public Repertoire(String dateOfMovies) throws ClassNotFoundException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
+            tdb = new TermsDB();
+            date = dateOfMovies;
+            hours = new Integer[12];
+            tdb.open();
+            movies = tdb.getTermsInDay(dateOfMovies);
+            tdb.close();
+            gettingInfo();
         }
         catch(ClassNotFoundException e){
             e.getMessage();
-        }
-        finally {
-            tdb = new TermsDB();
-            tdb.open();
-            this.dateOfMovie = dateOfMovies;
-            movies = tdb.getTermsInDay(dateOfMovies);
-            tdb.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
-    /*
-    public String gettingID() throws SQLException {
 
+    /**
+     * Metoda pobierajaca wszystkie informacje o filmie
+     *
+     * @throws SQLException
+     */
+
+
+    private void gettingInfo() throws SQLException {
         tdb.open();
-        String indexID = movies.getString("id");
+        Integer i = 0;
+        Integer j = 0;
+        while(movies.next()){
+            i+=1;
+        }
+        movieInfo = new String[i];
+        movies.beforeFirst();
+        while(movies.next()){
+            ResultSet temp = movies;
+            id = movies.getInt("id");
+            hall = movies.getInt("hall");
+            if(Arrays.binarySearch(movie, movies.getInt("movie"))>=0) {
+                movie[j] = movies.getInt("movie");
+            }
+            if(!mapMovieInfo.containsKey(movie[j])){
+                mapMovieInfo.put(movie[j],tdb.getMovieInfo(movie[j]));
+                int k = 0;
+                while(movies.next()){
+                    if(id==movies.getInt("id")){
+                        hours[k]=movies.getInt("id");
+                        k++;
+                    }
+                }
+                mapHours.put(movie[j], hours);
+                j++;
+            }
+            movies.beforeFirst();
+        }
+
         tdb.close();
-        return indexID;
     }
-    */
+
 }
--- src/Repertoire/Testowa.java
+++ src/Repertoire/Testowa.java
@@ -3,15 +3,14 @@ package Repertoire;
 import java.sql.SQLException;
 
 /**
- * Created by deleviretta on 23.03.16.
+ * Created on 23.03.16.
  */
 public class Testowa {
     public static void main(String[] args) throws SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Repertoire repertuar;
-            String date = new String("23.03.16");
-            repertuar = new Repertoire(date);
+            Repertoire repertuar = new Repertoire("23.03.16");
+
         } catch(ClassNotFoundException e)
         {
             e.getMessage();
