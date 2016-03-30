e355ee75de7728c59bbdfbae0c83ccca6e47fb67
BoyFarmer <BoyFarmerB@gmail.com>
2016-03-30 12:48:21 +0200

    Dodane okno z info o filmie

--- src/Windows/RightWindow.java
+++ src/Windows/RightWindow.java
@@ -12,27 +12,21 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.Image;
+import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Random;
 import java.util.Vector;
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.DefaultComboBoxModel;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -40,9 +34,11 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
-import javax.swing.plaf.basic.BasicComboBoxUI;
+import javax.swing.plaf.basic.BasicScrollBarUI;
 import javax.swing.table.JTableHeader;
 
 /**
@@ -61,49 +57,49 @@ public class RightWindow extends JPanel implements ActionListener {
     RightWindow() {
         StartWindow();
     }
-    public ArrayList<Integer> lista = new ArrayList<>();
-    public void StartWindow(){
+    private ArrayList<Integer> lista = new ArrayList<>();
+
+    public void StartWindow() {
         removeAll();
         setLayout(null);
         setBounds(WindowConstants.BORDER, 0, WindowConstants.WIDTH - WindowConstants.BORDER, WindowConstants.HEIGHT);
         setBackground(Color.black);
+
         MoviesDB db = new MoviesDB();
         db.open();
         ArrayList<Object[]> listaRes = new ArrayList<>();
         lista.clear();
         Random rand = new Random();
-        while(lista.size()<3){
-            int ID = rand.nextInt(db.getMaxId())+1;
-            if(lista.indexOf(ID)==-1){
+        while (lista.size() < 3) {
+            int ID = rand.nextInt(db.getMaxId()) + 1;
+            if (lista.indexOf(ID) == -1) {
                 lista.add(ID);
                 listaRes.add(db.getMovieInfo(ID));
             }
         }
         db.close();
-        
-        for(int i =0;i<3;i++){
+
+        for (int i = 0; i < 3; i++) {
+            final int ID = lista.get(i);
             Object[] res = listaRes.get(i);
-            JLabel labelIcon = new JLabel((ImageIcon)res[0]);
-            labelIcon.setBounds(51+(i*250), 66, 198, 284);
-            add(labelIcon);
-            JLabel opis = new JLabel((String)res[1]);
-            opis.setBounds(26+(i*250), 360, 198+50, 40);
-            opis.setForeground(Color.white);
-            opis.setHorizontalAlignment(SwingConstants.CENTER);
-            opis.setVerticalAlignment(SwingConstants.CENTER);
-            add(opis);
-            opis.addMouseListener(new MouseListener() {
+            JLabel labelIcon = new JLabel((ImageIcon) res[0]);
+            labelIcon.setBounds(51 + (i * 250), 66, 198, 284);
+
+            MouseListener aList = new MouseListener() {
+
                 @Override
                 public void mouseClicked(MouseEvent e) {
                     JLabel label = (JLabel) e.getSource();
-                    MainWindow.rightPanel.SchowInfoFilm(label.getText());
+                    MainWindow.rightPanel.SchowInfoFilm(ID);
                 }
 
                 @Override
-                public void mousePressed(MouseEvent e) {}
+                public void mousePressed(MouseEvent e) {
+                }
 
                 @Override
-                public void mouseReleased(MouseEvent e) {}
+                public void mouseReleased(MouseEvent e) {
+                }
 
                 @Override
                 public void mouseEntered(MouseEvent e) {
@@ -116,7 +112,17 @@ public class RightWindow extends JPanel implements ActionListener {
                     JLabel label = (JLabel) e.getSource();
                     label.setForeground(Color.white);
                 }
-            });
+            };
+
+            labelIcon.addMouseListener(aList);
+            add(labelIcon);
+            JLabel opis = new JLabel((String) res[1]);
+            opis.setBounds(26 + (i * 250), 360, 198 + 50, 40);
+            opis.setForeground(Color.white);
+            opis.setHorizontalAlignment(SwingConstants.CENTER);
+            opis.setVerticalAlignment(SwingConstants.CENTER);
+            add(opis);
+            opis.addMouseListener(aList);
         }
         repaint();
     }
@@ -175,7 +181,7 @@ public class RightWindow extends JPanel implements ActionListener {
                         public void mouseClicked(MouseEvent e) {
                             int row = jTable.rowAtPoint(e.getPoint());
                             int col = jTable.columnAtPoint(e.getPoint());
-                            System.out.println(row + " " + col+ " "+ jTable.getColumnName(col)+ " " );
+                            System.out.println(row + " " + col + " " + jTable.getColumnName(col) + " ");
                         }
 
                         @Override
@@ -217,7 +223,7 @@ public class RightWindow extends JPanel implements ActionListener {
     }
     private JTextField tf;
     private final Vector<String> v = new Vector<>();
-    private final JComboBox jtfSearch = new JComboBox();
+    private JComboBox jtfSearch = new JComboBox();
     private boolean hide_flag = false;
 
     public void MakeSearch() throws SQLException, ClassNotFoundException {
@@ -230,31 +236,19 @@ public class RightWindow extends JPanel implements ActionListener {
         MoviesDB mdb = new MoviesDB();
         mdb.open();
         Integer i = mdb.getMaxId();
-        /*while(mdb.getTitle(i+1)!=null){
-            i++;
-        }*/
         String[] str = new String[i];
         for (int j = 0; j < i; j++) {
             str[j] = mdb.getTitle(j + 1);
         }
+        mdb.close();
         jtfSearch.setEditable(true);
         //jtfSearch.setModel(new DefaultComboBoxModel(str));
-        jtfSearch.setBounds(150, (int) (WindowConstants.HEIGHT * 0.3), WindowConstants.WIDTH - WindowConstants.BORDER - 300, 100);
+        //jtfSearch.setBounds(150, (int) (WindowConstants.HEIGHT * 0.3), WindowConstants.WIDTH - WindowConstants.BORDER - 300, 100);
         //jtfSearch.setFont(new Font("Arial Black", Font.CENTER_BASELINE, 36));
         //jtfSearch.setForeground(Color.WHITE);
         //jtfSearch.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 3));
         //jtfSearch.setBackground(new Color(104, 158, 150));
-        jtfSearch.setUI(new BasicComboBoxUI() { ///////////usuwa widoczne rozwijanie z JComboBox
-            @Override
-            protected JButton createArrowButton() {
-                return new JButton() {
-                    @Override
-                    public int getWidth() {
-                        return 0;
-                    }
-                };
-            }
-        });
+
         tf = (JTextField) jtfSearch.getEditor().getEditorComponent();
         tf.addKeyListener(new KeyAdapter() {
             @Override
@@ -313,6 +307,8 @@ public class RightWindow extends JPanel implements ActionListener {
         setModel(new DefaultComboBoxModel(v), "");
         JPanel p = new JPanel(new BorderLayout());
         p.add(jtfSearch, null);
+        p.setBounds(150, (int) (WindowConstants.HEIGHT * 0.3), WindowConstants.WIDTH - WindowConstants.BORDER - 300, 100);
+
         add(p);
         setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         setPreferredSize(new Dimension(300, 150));
@@ -321,17 +317,27 @@ public class RightWindow extends JPanel implements ActionListener {
         ibSearch = new ImageButton("res/Szukaj.png");
         ibSearch.setRolloverIcon(new ImageIcon("res/SzukajEntered.png"));
         ibSearch.setBounds(210, (int) (WindowConstants.HEIGHT * 0.3) + 130, WindowConstants.WIDTH - WindowConstants.BORDER - 420, 80);
-        ibSearch.addActionListener(new ActionListener(){
+        ibSearch.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println(jtfSearch.getEditor().getItem().toString());
             }
-            
+
         });
         add(ibSearch);
-
-        add(jtfSearch);
-        setBounds(WindowConstants.BORDER, 0, WindowConstants.WIDTH - WindowConstants.BORDER, WindowConstants.HEIGHT);
+        /*jtfSearch.setUI(new BasicComboBoxUI() { ///////////usuwa widoczne rozwijanie z JComboBox
+            @Override                           ///////////Nie radze zmienaic miejsca wywołania setUI
+            protected JButton createArrowButton() {
+                return new JButton() {
+                    @Override
+                    public int getWidth() {
+                        return 0;
+                    }
+                };
+            }
+        });*/
+        //add(jtfSearch);
+        //setBounds(WindowConstants.BORDER, 0, WindowConstants.WIDTH - WindowConstants.BORDER, WindowConstants.HEIGHT);
 
         repaint();
         mdb.close();
@@ -359,7 +365,112 @@ public class RightWindow extends JPanel implements ActionListener {
 
     }
 
-    private void SchowInfoFilm(String source) {
-        System.out.println("Tu bedzą informacje o "+ source);
+    /*
+                0 - obrazek
+                !1 - tytuł
+                !2 - gatunek
+                !3 - długość
+                !4 - język 
+                !5 - wiek
+                !6 - reżyser
+                !7 - aktorzy
+                !8 - kraj
+                !9 - rok prod
+                10 - ocena
+                11 - opis
+     */
+    private void SchowInfoFilm(int ID) {
+        removeAll();
+        setLayout(null);
+        setBackground(Color.BLACK);
+        setBounds(WindowConstants.BORDER, 0, WindowConstants.WIDTH - WindowConstants.BORDER, WindowConstants.HEIGHT);
+
+        MoviesDB mdb = new MoviesDB();
+        mdb.open();
+        Object[] res = mdb.getMovieInfo(ID);
+        mdb.close();
+        JLabel labelIcon = new JLabel((ImageIcon) res[0]);
+        labelIcon.setBounds(51, 66, 198, 284);
+        add(labelIcon);
+
+        JLabel[] infoName = new JLabel[9];
+        infoName[0] = new JLabel("Tytuł:");
+        infoName[1] = new JLabel("Gatunek:");
+        infoName[2] = new JLabel("Długość:");
+        infoName[3] = new JLabel("Język:");
+        infoName[4] = new JLabel("Min wiek:");
+        infoName[5] = new JLabel("Reżyseria:");
+        infoName[6] = new JLabel("Obsada:");
+        infoName[7] = new JLabel("Kraj:");
+        infoName[8] = new JLabel("Rok produkcji:");
+        JLabel[] infoFilm = new JLabel[9];
+        for (int i = 0; i < infoName.length; i++) {
+            infoName[i].setBounds(260, 66 + (i * 32), 110, 30);
+            infoName[i].setForeground(new Color(247, 214, 185));
+            infoName[i].setFont(new Font("Arial Black", Font.CENTER_BASELINE, 12));
+            add(infoName[i]);
+
+            infoFilm[i] = new JLabel(res[i + 1].toString());
+            infoFilm[i].setBounds(380, 66 + (i * 32), 400, 30);
+            infoFilm[i].setFont(new Font("Arial Black", Font.CENTER_BASELINE, 11));
+            infoFilm[i].setForeground(Color.white);
+            add(infoFilm[i]);
+        }
+
+        JTextArea filmDescription = new JTextArea(res[11].toString());
+ 
+        filmDescription.setWrapStyleWord(true);
+        filmDescription.setLineWrap(true);
+        filmDescription.setEditable(false);
+        filmDescription.setFocusable(false);
+        //filmDescription.setOpaque(false);
+        
+        
+        filmDescription.setEditable(false);
+        filmDescription.setFont(new Font("Arial Black", Font.CENTER_BASELINE, 11));
+        filmDescription.setForeground(Color.white);
+        filmDescription.setBounds(0, 0, 490, 150);
+        filmDescription.setBackground(Color.black);
+        filmDescription.setBorder(BorderFactory.createLineBorder(Color.black, 1));
+
+        JScrollPane scroll =new JScrollPane(filmDescription,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+        );
+        scroll.setBounds(260, 354, 530, 210);
+        scroll.setBorder(BorderFactory.createLineBorder(Color.black, 1));
+        scroll.getVerticalScrollBar().setUI(new BasicScrollBarUI(){
+        @Override
+        protected JButton createDecreaseButton(int orientation) {
+            return createZeroButton();
+        }
+
+        @Override    
+        protected JButton createIncreaseButton(int orientation) {
+              return createZeroButton();
+        }
+        @Override 
+        protected void configureScrollBarColors(){
+            this.trackColor = Color.black;
+            thumbColor = new Color(84,54,54);
+
+        }
+        private JButton createZeroButton() {
+            JButton jbutton = new JButton();
+            jbutton.setPreferredSize(new Dimension(0, 0));
+            jbutton.setMinimumSize(new Dimension(0, 0));
+            jbutton.setMaximumSize(new Dimension(0, 0));
+            return jbutton;
+        }
+        });
+
+        GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+        gbc_scrollPane.fill = GridBagConstraints.BOTH;
+        gbc_scrollPane.gridx = 0;
+        gbc_scrollPane.gridy = 0;
+        add(scroll, gbc_scrollPane);
+
+
+        repaint();
     }
 }
