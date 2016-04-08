9734ead4cb150c71f317c84bd9b2a3454a9b4991
kamiloleszek <oleszek101@wp.pl>
2016-04-08 11:42:52 +0200

    Klasa obslugujaca rezerwacje, klasy pomocnicze: bilet, klient, miejsce, film

---
+++ src/Booking/Hall.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Booking;
+
+/**
+ *
+ * @author Kamil Oleszek
+ */
+public class Hall {
+    public final int ROW = 10;
+    public final int COLUMN = 20;
+    int[][] Sala1 = new int[ROW][COLUMN];
+    
+    
+}
---
+++ src/Booking/Ticket.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Booking;
+
+/**
+ *
+ * @author Kamil Oleszek
+ */
+public class Ticket {
+    private String type;
+    private int amount;
+    private double price;
+    private double sum;
+    static int total_amount;
+    static double total_sum;
+    static String[] types = {"Normalny", "Szkolny", "Seniorski", "Stsudencki"} ;
+    Ticket(int index){
+        type = types[index];
+        switch(index){
+            case 0: 
+                price = 24.00;
+                break;
+            case 3:
+            case 1:
+                price = 17.00;
+                break;
+            case 2:
+                price = 18.00;
+                break;
+        }
+        ++total_amount;
+    }
+}
