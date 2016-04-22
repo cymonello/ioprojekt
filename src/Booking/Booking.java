7d0009249fb18264ee0dc81e44e60521ab0cca2b
kamiloleszek <oleszek101@wp.pl>
2016-04-22 12:27:49 +0200

    method ceny()

--- src/Booking/Booking.java
+++ src/Booking/Booking.java
@@ -54,7 +54,7 @@ public class Booking {
     
     public double[] ceny(){
         double[] ceny = new double[4];
-        ceny[0] = 24.00;
+        ceny[0] = 24.05;
         ceny[1] = 17.00;
         ceny[2] = 18.00;
         ceny[3] = 17.00;
